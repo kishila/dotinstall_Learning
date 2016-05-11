16266e62216bbf09b128fe7f6950d216ae4ee137
中井智己 <s12t282@stmail.eng.kagawa-u.ac.jp>
2016-05-10 21:49:19 -0700

    lesson#08

--- Sinatra/sinatra_lessons/main.rb
+++ Sinatra/sinatra_lessons/main.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/:name' do |n|
-  @name = n
+get '/' do
   @title = "main index"
+  @content = "main content"
   erb :index
+end
+
+get '/about' do
+  @title = "about this page"
+  @content = "this page is ..."
+  @email = "taguchi@gmail.com"
+  erb :about
 end
