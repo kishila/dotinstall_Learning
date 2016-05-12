944189bcfd0cd76f65a9d7d27cfe18b237860317
中井智己 <s12t282@stmail.eng.kagawa-u.ac.jp>
2016-05-11 21:07:02 -0700

    lesson#9

--- Sinatra/sinatra_lessons/main.rb
+++ Sinatra/sinatra_lessons/main.rb
@@ -1,15 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+before do
+    @author = "taguchi"
+end
+
+after do
+  logger.info "page displayed successfully"
+end
+
 get '/' do
   @title = "main index"
-  @content = "main content"
+  @content = "main content by " + @author
   erb :index
 end
 
 get '/about' do
   @title = "about this page"
-  @content = "this page is ..."
+  @content = "this page is ... by " + @author
   @email = "taguchi@gmail.com"
   erb :about
 end
