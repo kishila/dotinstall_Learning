7aa94c1a1f529b675e753fdc5462bb93739c33cf
中井智己 <s12t282@stmail.eng.kagawa-u.ac.jp>
2016-05-12 14:22:30 +0900

    lesson#10

--- Sinatra/sinatra_lessons/main.rb
+++ Sinatra/sinatra_lessons/main.rb
@@ -9,6 +9,12 @@ after do
   logger.info "page displayed successfully"
 end
 
+helpers do
+  def strong(s)
+    "<strong>#{s}</strong>"
+  end
+end
+
 get '/' do
   @title = "main index"
   @content = "main content by " + @author
@@ -17,7 +23,7 @@ end
 
 get '/about' do
   @title = "about this page"
-  @content = "this page is ... by " + @author
+  @content = "this page is ... by " + strong(@author)
   @email = "taguchi@gmail.com"
   erb :about
 end
