dfb76a018da74985c3fdc3e9ddeea08bb57fab70
中井智己 <s12t282@stmail.eng.kagawa-u.ac.jp>
2016-05-13 21:22:34 +0900

    lesson#11

--- Sinatra/sinatra_lessons/main.rb
+++ Sinatra/sinatra_lessons/main.rb
@@ -1,29 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-before do
-    @author = "taguchi"
-end
-
-after do
-  logger.info "page displayed successfully"
-end
-
-helpers do
-  def strong(s)
-    "<strong>#{s}</strong>"
-  end
-end
-
 get '/' do
-  @title = "main index"
-  @content = "main content by " + @author
   erb :index
-end
-
-get '/about' do
-  @title = "about this page"
-  @content = "this page is ... by " + strong(@author)
-  @email = "taguchi@gmail.com"
-  erb :about
 end
