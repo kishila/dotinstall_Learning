9f96657b1a2a4194201957793e1a1d189b45efc9
中井智己 <s16g459@stu.kagawa-u.ac.jp>
2016-05-28 23:15:26 +0900

    lesson#12

--- Sinatra/sinatra_lessons/main.rb
+++ Sinatra/sinatra_lessons/main.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
+)
+
+class Comments < ActiveRecord::Base
+end
 
 get '/' do
+  @comments = Comments.order("id desc").all
   erb :index
 end
