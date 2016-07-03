7f14c32d280aed7fae45ba153807ed23e8b370ac
中井智己 <s16g459@stu.kagawa-u.ac.jp>
2016-07-04 02:05:17 +0900

    lesson#10

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,20 +1,22 @@
 var http = require('http');
   fs = require('fs');
+  ejs = require('ejs');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
+var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+var n = 0;
 
 server.on('request', function(req, res) {
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, date) {
-    if (err) {
-      res.writeHead(404, {'Cotent-Type': 'text/plain'});
-      res.write("not found!");
-      return res.end();
-    }
-    res.writeHead(200, {'Cotent-Type': 'text/html'});
-    res.write(date);
-    res.end();
+  n++;
+  var date = ejs.render(template, {
+    title: "hello",
+    content: "<strong>World!</strong>",
+    n: n
   });
+  res.writeHead(200, {'Cotent-Type': 'text/html'});
+  res.write(date);
+  res.end();
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
