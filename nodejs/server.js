d87f52ee5ddf49282684ddafbea29685406d9b36
中井智己 <s16g459@stu.kagawa-u.ac.jp>
2016-07-03 19:39:48 +0900

    lesson#08

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,23 +1,20 @@
 var http = require('http');
+  fs = require('fs');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
 
 server.on('request', function(req, res) {
-  switch (req.url) {
-    case '/about':
-      msg = "about this page";
-      break;
-    case '/profile':
-      msg = "about me";
-      break;
-    default:
-      msg = "wrong page";
-      break;
-  }
-  res.writeHead(200, {'Cotent-Type': 'text/plain'});
-  res.write(msg);
-  res.end();
+  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, date) {
+    if (err) {
+      res.writeHead(404, {'Cotent-Type': 'text/plain'});
+      res.write("not found!");
+      return res.end();
+    }
+    res.writeHead(200, {'Cotent-Type': 'text/html'});
+    res.write(date);
+    res.end();
+  });
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
