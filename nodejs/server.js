27ce9ee98fa2fb80bf12322d0c5b94c7e350e8a1
中井智己 <s16g459@stu.kagawa-u.ac.jp>
2016-07-04 02:16:45 +0900

    lesson#12

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,22 +1,33 @@
 var http = require('http');
   fs = require('fs');
   ejs = require('ejs');
+  qs = require('querystring');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-var n = 0;
-
-server.on('request', function(req, res) {
-  n++;
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+var posts = [];
+function renderForm(posts, res) {
   var date = ejs.render(template, {
-    title: "hello",
-    content: "<strong>World!</strong>",
-    n: n
+    posts: posts
   });
   res.writeHead(200, {'Cotent-Type': 'text/html'});
   res.write(date);
   res.end();
+}
+
+server.on('request', function(req, res) {
+  if(req.method === 'Post'){
+    req.date = "";
+    req.on("readable", function() {
+      req.date += read.read();
+    });
+    req.on("end", function(){
+
+    })
+  } else {
+    renderForm(posts, res);
+  }
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
