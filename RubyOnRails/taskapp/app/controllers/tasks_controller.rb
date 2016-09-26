736133926f01523a2d693ec3338a6073b9934179
中井智己 <s16g459@stu.kagawa-u.ac.jp>
2016-09-27 00:30:16 +0900

    lesson#23

--- RubyOnRails/taskapp/app/controllers/tasks_controller.rb
+++ RubyOnRails/taskapp/app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+  def create
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.create(tasks_params)
+    redirect_to project_path(@project.id)
+  end
+
+  private
+    def tasks_params
+      params[:task].permit(:title)
+    end
 end
